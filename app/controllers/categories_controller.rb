3972d11f7f28696fa9c63a5f995c3bbf889e38ac
Azim Can <azim.can.kuruca@lab2023.com>
2025-02-11 16:51:51 +0300

    Add update and destroy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,11 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @categories = Category.all
+  end
+
+  def show
   end
 
   def new
@@ -10,24 +15,33 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to categories_path
+      redirect_to categories_path, notice: "Kayıt başarılı!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "Güncellendi"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "Silindi"
   end
 
   private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
     def category_params
-      # params.require(:category).permit(:title)
       params.expect(category: [ :title ])
     end
 end
