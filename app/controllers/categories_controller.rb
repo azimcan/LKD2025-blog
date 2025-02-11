8c2f634ae76fe30b5521bebfefefe8950b320fbf
Azim Can <azim.can.kuruca@lab2023.com>
2025-02-11 19:27:37 +0300

    Update ui

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,8 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_category, only: [ :edit, :update, :destroy ]
 
   def index
     @categories = Category.all
-  end
-
-  def show
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories, only: [ :index, :new, :create, :edit, :update, :destroy ]
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
